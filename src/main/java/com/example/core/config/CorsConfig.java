e43fc16e9f81a433e6c5134252dd82d76405ac07
Timur <ggg255428@mail.ru>
2025-11-20 02:47:54 +0500

    Внедрение Cors(CorsConfig)

---
+++ src/main/java/com/example/core/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.example.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",           // локальный фронтенд
+                                "https://musoren-front.vercel.app" // продакшен-фронтенд
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization") // если используешь JWT
+                        .allowCredentials(true); // если нужен доступ к cookies
+            }
+
+        };
+    }
+}
